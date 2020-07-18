84e478e4ac550af89105c4af9e45ba979910a9fa
Ahmed Bahnasy <ahmed.bahnasy@tum.de>
2020-07-18 15:54:41 +0200

    ProblemA solution

--- week_13/ProblemA.cpp
+++ week_13/ProblemA.cpp
@@ -5,54 +5,7 @@
 
 using namespace std;
 
-struct Edge{
-    int dir, w;
-};
-void addEdge(vector<Edge> adj[], int s, int d, int w) {
-    adj[s].push_back({d,w});
-    adj[d].push_back({s,w});
-}
 
-void find_paths(vector<vector<int>>& parent, vector<vector<int>>& paths,
-        vector<int>& curr_path,int node) {
-    if(node == -1) {
-        // base case
-        paths.push_back(curr_path);
-        return;
-    }
-    for (auto x : parent[node]) {
-        curr_path.push_back(node);
-        find_paths(parent, paths, curr_path, x);
-        curr_path.pop_back();
-    }
-}
-
-void find_shortest_paths (vector<vector<int>>& g, int s, int d, vector<vector<int>>& parent, int V) {
-
-    vector<int> dist(V, INT_MAX);
-    queue<int> q;
-
-    parent[s].push_back(-1);
-    dist[s] = 0;
-    q.push(s);
-    while (!q.empty()) {
-        int curr = q.front();
-        q.pop();
-        for (int i = 0; i < V; ++i) {
-            if (g[curr][i] != -1) { // is neighbour
-                if (dist[i] > dist[curr] + g[curr][i]) {
-                    dist[i] = dist[curr] + g[curr][i];
-                    parent[i].clear();
-                    parent[i].push_back(curr);
-                    q.push(i);
-                } else if (dist[i] == dist[curr] + g[curr][i]) {
-                    parent[i].push_back(curr);
-                }
-            }
-        }
-//
-    }
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -68,43 +21,72 @@ int main() {
     for (int t = 1; t <= test_cases; ++t) {
         cout << "Case #" << t << ": ";
         cin >> V >> P;
-//        vector<Edge> adj[V];
-        vector<vector<int>> g(V, vector<int>(V, -1));
-        vector<vector<int>> g_duplicate(V, vector<int>(V, 1));
+
+        vector<vector<pair<int, int>>> adj(V);
         vector<vector<int>> parent(V);
+        vector<int> dist (V, INT_MAX);
+        vector<bool> vis(V, false);
+        vector<vector<int>> g_w(V, vector<int>(V, -1));
+
 
         int s, d, w;
         for (int i = 0; i < P; ++i) {
             cin >> s>> d >> w;
-            if(g[s][d] == -1 || g[s][d] > w) {
-                g[s][d] = w;
-                g[d][s] = w;
-            } else if (g[s][d] == w){
-                g_duplicate[s][d] += 1;
-                g_duplicate[d][s] += 1;
+            if (s == d) // skip self loops
+                continue;
+            adj[s].push_back({w,d});
+            adj[d].push_back({w,s}); // undirected
+
+            if(g_w[s][d] == -1 || g_w[s][d] > w) {
+                g_w[s][d] = w;
+                g_w[d][s] = w;
+            }
+
+        }
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0,0});
+        dist[0] = 0;
+
+        while (!pq.empty()) {
+            int curr = pq.top().second; // get node index
+            pq.pop();
+            if(!vis[curr]) {
+                vis[curr] = true;
+                for (auto i: adj[curr]) {
+                    int next = i.second;
+                    int w = i.first;
+
+                    if (dist[next] > dist[curr] + w) {
+                        dist[next] = dist[curr] + w;
+                        parent[next].clear();
+                        parent[next].push_back(curr);
+                        pq.push({dist[curr]+w, next});
+                    } else if (dist[next] == dist[curr] + w) {
+                        parent[next].push_back(curr);
+                    }
+
+                }
             }
         }
 
-        vector<vector<int>> paths;
-        vector<int> path;
-        find_shortest_paths(g, 0, V-1, parent, V);
-        find_paths(parent, paths, path, V-1);
-        vector<vector<bool>> g_taken(V, vector<bool>(V, false));
+        vis.clear();
+        vis.resize(V, false);
+        queue<int> q;
+        q.push(V-1);
         int sum = 0;
-        for (auto v : paths) {
-            reverse(v.begin(), v.end());
-            // Print node for the current path
-            for(int i = 1; i < v.size(); ++i) {
-                   if(g_taken[v[i-1]][v[i]] == false && g_taken[v[i]][v[i-1]] == false) {
-                       for(int j = 0; j < g_duplicate[v[i-1]][v[i]]; ++j) {
-                           sum += g[v[i-1]][v[i]];
-//                           cout << "adding " << i-1 << " to " << i << "\n";
-                       }
-                       g_taken[v[i-1]][v[i]] = true;
-                       g_taken[v[i]][v[i-1]] = true;
-                   }
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            if(!vis[curr]) {
+                vis[curr] = true;
+                for (auto i: parent[curr]) {
+                    sum += g_w[curr][i];
+                    q.push(i);
+                }
             }
         }
+
         cout << sum*2 << "\n";
 
 
